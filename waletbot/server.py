31d9f6c027fa54f95fa625d3f2bb0de51f3e948c
ffers <fferses@gmail.com>
2020-05-10 13:11:22 +0300

    set denied

--- waletbot/server.py
+++ waletbot/server.py
@@ -7,18 +7,20 @@ from aiogram import Bot, Dispatcher, executor, types
 
 import exceptions
 import expenses
+import misc
 from categories import Categories
 from middlewares import AccessMiddleware
 
+token = misc.token
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
+API_TOKEN = os.getenv(token)
+#PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+#PROXY_AUTH = aiohttp.BasicAuth(
+#    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
+#    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
+#)
 ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 
 bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
